1a0b1d2768c24735555778772916db9a53250d15
Mikhail Makartsev <m.p.makartsev@gmail.com>
2023-09-25 16:20:30 +0600

    Fix 2nd home task
    Extracted logic in the 'main' method to separate methods.

--- app/src/main/java/fintech/App.java
+++ app/src/main/java/fintech/App.java
@@ -10,7 +10,25 @@ import java.util.Map;
 public class App {
     public static void main(String[] args) {
         List<Weather> weatherList = new ArrayList<>();
+        fillWeatherList(weatherList);
 
+        Map<String, Double> averageTemperatureByRegion = getAverageTemperatureByRegion(weatherList);
+        System.out.println("Average temperature by region:\n" +
+                averageTemperatureByRegion + "\n");
+
+        List<String> regionsWhereTemperatureWasMoreThan30 = getRegionsWhereTemperatureWasMoreThan(weatherList, 30.0);
+        System.out.println("Regions where temperature was more than 30:\n" +
+                regionsWhereTemperatureWasMoreThan30 + "\n");
+
+        Map<Integer, List<Double>> temperatureListById = getTemperatureListById(weatherList);
+        System.out.println("Temperatures by id\n" + temperatureListById + "\n");
+
+        Map<Double, List<Weather>> temperatureToWeatherList = getTemperatureToWeatherList(weatherList);
+        System.out.println("Weather items by temperature:\n" +
+                temperatureToWeatherList + "\n");
+    }
+
+    private static void fillWeatherList(List<Weather> weatherList) {
         weatherList.add(new Weather(52, "Omskaya oblast'", 38.9,
                 LocalDateTime.of(2023, 7, 12, 12, 0)));
         weatherList.add(new Weather(52, "Omskaya oblast'", -12.2,
@@ -29,29 +47,30 @@ public class App {
                 LocalDateTime.of(2022, 11, 16, 0, 0)));
         weatherList.add(new Weather(25, "Irkutskaya oblast'", -15.5,
                 LocalDateTime.of(2022, 11, 26, 0, 0)));
+    }
 
-        Map<String, Double> averageTemperatureByRegion = weatherList.stream().collect(
+    private static Map<String, Double> getAverageTemperatureByRegion(List<Weather> weatherList) {
+        return weatherList.stream().collect(
                 groupingBy(Weather::getRegionName,
                         averagingDouble(Weather::getTemperatureInCelsius)));
-        System.out.println("Average temperature by region:\n" +
-                averageTemperatureByRegion + "\n");
+    }
 
-        List<String> regionsWhereTemperatureWasMoreThan30 = weatherList.stream()
-                .filter(weather -> weather.getTemperatureInCelsius() > 30.0)
+    private static List<String> getRegionsWhereTemperatureWasMoreThan(List<Weather> weatherList,
+            double temperature) {
+        return weatherList.stream()
+                .filter(weather -> weather.getTemperatureInCelsius() > temperature)
                 .map(Weather::getRegionName)
                 .distinct()
                 .toList();
-        System.out.println("Regions where temperature was more than 30:\n" +
-                regionsWhereTemperatureWasMoreThan30 + "\n");
+    }
 
-        Map<Integer, List<Double>> temperatureListById = weatherList.stream().collect(
-                groupingBy(Weather::getId,
-                        mapping(Weather::getTemperatureInCelsius, toList())));
-        System.out.println("Temperatures by id\n" + temperatureListById + "\n");
+    private static Map<Integer, List<Double>> getTemperatureListById(List<Weather> weatherList) {
+        return weatherList.stream().collect(groupingBy(
+                Weather::getId, mapping(Weather::getTemperatureInCelsius, toList())));
+    }
 
-        Map<Double, List<Weather>> temperatureToWeatherList = weatherList.stream().collect(
+    private static Map<Double, List<Weather>> getTemperatureToWeatherList(List<Weather> weatherList) {
+        return weatherList.stream().collect(
                 groupingBy(Weather::getTemperatureInCelsius));
-        System.out.println("Weather items by temperature:\n" +
-                temperatureToWeatherList + "\n");
     }
 }
