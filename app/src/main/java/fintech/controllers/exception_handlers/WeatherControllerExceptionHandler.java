4cccb813562e49ca8157105566f251d016c05692
Mikhail Makartsev <m.p.makartsev@gmail.com>
2023-10-05 04:14:00 +0600

    Change ResponseEntity creation way

--- app/src/main/java/fintech/controllers/exception_handlers/WeatherControllerExceptionHandler.java
+++ app/src/main/java/fintech/controllers/exception_handlers/WeatherControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package fintech.controllers.exception_handlers;
 
+import fintech.controllers.WeatherController;
 import fintech.exceptions.IncorrectDateAndTimeFormatException;
 import fintech.exceptions.WeatherNotFoundException;
 import org.springframework.http.HttpStatus;
@@ -8,18 +9,18 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@RestControllerAdvice(assignableTypes = WeatherController.class)
 public class WeatherControllerExceptionHandler {
 
     @ExceptionHandler(IncorrectDateAndTimeFormatException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<String> handleIncorrectDateAndTime(RuntimeException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<String> handleIncorrectDateAndTime(IncorrectDateAndTimeFormatException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
     @ExceptionHandler(WeatherNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<String> handleWeatherAbsence(RuntimeException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 }
