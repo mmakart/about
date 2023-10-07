e26532f837b27baa76ce7568616b7bcff42f77b9
Mikhail Makartsev <m.p.makartsev@gmail.com>
2023-10-07 23:19:59 +0600

    Add Swagger docs for response codes and input vars

--- app/src/main/java/fintech/controllers/WeatherApiDotComClientController.java
+++ app/src/main/java/fintech/controllers/WeatherApiDotComClientController.java
@@ -5,6 +5,11 @@ import fintech.dto.WeatherApiDotComErrorMessageDto;
 import fintech.exceptions.web_client.WebClientBaseException;
 import fintech.exceptions.web_client.factory.WebClientExceptionFactory;
 import io.github.resilience4j.ratelimiter.RateLimiter;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.function.Function;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -29,8 +34,19 @@ public class WeatherApiDotComClientController {
     @Qualifier("standardWebApiDotComClientRateLimiter")
     private final RateLimiter rateLimiter;
 
+    @Operation(summary = "Get current weather by city name")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Found the weather", content = {
+                    @Content(mediaType = "application/json") }),
+            @ApiResponse(responseCode = "400", description = "Wrong request"),
+            @ApiResponse(responseCode = "403", description = "Problems with the API token"),
+            @ApiResponse(responseCode = "404", description = "Requested city not found"),
+            @ApiResponse(responseCode = "429", description = "Request limit exceeded"),
+            @ApiResponse(responseCode = "500", description = "Unknown or some internal error occurred")
+    })
     @GetMapping("/now/{city}")
-    public Mono<Object> getCurrentWeatherForCity(@PathVariable("city") String city) {
+    public Mono<Object> getCurrentWeatherForCity(
+            @Parameter(description = "The city to get weather for") @PathVariable("city") String city) {
         Function<String, Mono<Object>> function = innerCity -> client.get()
                 .uri(uriBuilder -> uriBuilder.queryParam("key", props.getAuthKey())
                         .queryParam("q", innerCity)
